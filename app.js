a8adf7878f78b0bf14036426f85c726620125b43
Shiva <SRamasubramanian@slb.com>
2018-07-26 17:07:50 -0500

    Just before completing Reusable Components chapter

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 class Button extends React.Component {
-	state = { counter: 0 };
 
-  handleClick = () => {
-  	this.setState ((prevState) => ({
-      	counter: prevState.counter + 1
-      }));
+    // handleClick = () => {
+    //   this.setState ((prevState) => ({
+    //       counter: prevState.counter + 1
+    //     }));
+    // };
+
+    render() {
+      return (
+        <button
+            onClick={()=>this.props.onClickFunction(this.props.incrementValue)}>
+          +{this.props.incrementValue}
+        </button>
+      );
+    }
+  }
+
+  const Result = (props) => {
+    return (
+      <div>{props.counter}</div>
+    );
   };
 
-	render() {
-  	return (
-    	<button onClick={this.handleClick}>
-      	{this.state.counter}
-      </button>
-		);
-  }
-}
+  class App extends React.Component {
+      state = { counter: 0 };
 
-ReactDOM.render(<Button />, mountNode);
+    incrementCounter = (incrementValue) => {
+          this.setState ((prevState) => ({
+              counter: prevState.counter + incrementValue
+          }));
+    }
+
+    render() {
+      return (
+        <div>
+          <Button incrementValue={1} onClickFunction={this.incrementCounter}/>
+          <Button incrementValue={5} onClickFunction={this.incrementCounter}/>
+          <Button incrementValue={10} onClickFunction={this.incrementCounter}/>
+          <Button incrementValue={100} onClickFunction={this.incrementCounter}/>
+          <Result counter={this.state.counter}/>
+        </div>
+      );
+    }
+
+  }
+
+  ReactDOM.render(<App />, mountNode);
